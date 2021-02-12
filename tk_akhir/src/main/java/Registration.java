de5230ede0ead282aa20de8d0c8429fa6166fdae
Bharata Gangga <bharataganggakrishnayudha@gmail.com>
2021-02-12 14:14:41 +0700

    Add files via upload

---
+++ tk_akhir/src/main/java/Registration.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author bhara
+ */
+class Registration {
+    public static void main(String[] args)
+                          throws Exception
+    {
+        MyFrame f = new MyFrame();
+    }
+}
